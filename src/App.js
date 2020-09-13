4a524e0c0a1cf697c034a404aee363cac50651ab
patrick-fv <patrick.fv@outlook.com>
2020-09-13 20:12:12 -0300

    implement Genres table

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Header from './Header'
-
-const Generos = () => {
-  return (
-    <div>
-      Generos
-    </div>
-  )
-}
-
+import Genres from './Genres'
+import axios from 'axios'
 
 const App = () => {
+  const [data, setData] = useState({})
+  useEffect(() => {
+    axios.get('/api').then((res) => {
+      setData(res.data)
+      console.log(data);
+    })
+  }, [])
+
   return (
     <BrowserRouter>
       <div className='App'>
         <Header />
-        <Route path='/generos' exact component={Generos}/>
+        <Route path='/generos' exact component={Genres}/>
       </div>
     </BrowserRouter>
   )
--- src/Genres.js
+++ src/Genres.js
@@ -3,16 +3,45 @@ import axios from 'axios'
 
 const Genres = () => {
     const [data, setData] = useState([])
+    const renderLine = (record) => {
+        return (
+            <tr key={record.id}>
+                <th scope='row'>{record.id}</th>
+                <td>{record.name}</td>
+                <td><button>+</button></td>
+            </tr>
+        )
+    }
     useEffect(() => {
         axios.get('/api/genres')
-            .then((res) => {
-                setData(res.data.data)
-            })
+        .then((res) => {
+            //setData(res.data.data)
+        })
     }, [])
+
+    if(data.length === 0) {
+        return (
+            <div class="alert alert-warning" role="alert">
+                Você não possui genêros criados!
+            </div>
+        )
+    }
 
     return (
         <div>
-            Genêros<pre>{JSON.stringify(data)}</pre>
+        <h1>Genêro</h1>
+        <table className='table table-dark'>
+            <thead>
+                <tr>
+                    <th scope='col'>ID</th>
+                    <th scope='col'>Nome</th>
+                    <th scope='col'>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.map(renderLine)}
+            </tbody>
+        </table>
         </div>
     )
 }
