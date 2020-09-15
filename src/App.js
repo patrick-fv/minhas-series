33d9c3580be20f6ae84e9dfac04cf9c0f6ef40f7
patrick-fv <patrick.fv@outlook.com>
2020-09-14 22:55:16 -0300

    refactor layout App and Header components

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ const App = () => {
 
   return (
     <BrowserRouter>
-      <div>
-        <Header/>
+    <Header/>
+      <div className='container'>
         <Switch>
           <Route path='/generos' exact component={Genres} />
           <Route path='/generos/novo' component={NewGenre} />
--- src/Header.js
+++ src/Header.js
@@ -18,15 +18,20 @@ const Header = () => {
     return (
       <div>
         <Navbar color='light' light expand='md'>
+          <div className='container'>
             <NavbarBrand tag={Link} to='/'>Minhas Séries</NavbarBrand>
             <NavbarToggler onClick={toggle}/>
             <Collapse isOpen={isOpen} navbar>
-                <Nav className='mr-auto' navbar>
+                <Nav className='ml-auto' navbar >
+                <NavItem>
+                    <NavLink tag={Link} to='/series' >Séries</NavLink>
+                </NavItem>
                 <NavItem>
                     <NavLink tag={Link} to='/generos' >Genêros</NavLink>
                 </NavItem>
                 </Nav>
           </Collapse>
+          </div>
         </Navbar>
       </div>
     )  
