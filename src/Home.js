639be06ad290fc4c79347c941db85b36ac46872e
patrick-fv <patrick.fv@outlook.com>
2020-10-18 22:07:59 -0300

    create HeaderHome component

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
+const HeaderHome = () => {
+  return (
+    <header className='jumbotron my-4'>
+      <h1>Minhas Séries Favoritas</h1>
+      <p className='lead'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel fugit consectetur voluptatibus sed, voluptates modi, obcaecati accusamus nemo incidunt dicta similique consequatur natus dolor porro. Fugit numquam voluptatem corrupti enim.
+      </p>
+    </header>
+  )
+}
+
 const Home = () => {
   return (
     <div className='container'>
-      <h1>Home</h1>
+      <HeaderHome/>
     </div>
   ) 
 }
